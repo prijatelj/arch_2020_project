0515fb0913e911f2edcb0823ed35eb6ab2ee8451
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2020-04-13 16:23:05 -0400

    little script 	new file:   script-python.sh

---
+++ script-python.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+for f in ~/repos/arch_2020_project/trace_files/*
+do
+    for m in {0..6}
+    do
+        for n in {1..2}
+        do
+            (python3 baselines/python/branchsim.py -f $f -m $m -n $n -k 8)
+        done
+    done
+done

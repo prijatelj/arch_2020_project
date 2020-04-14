aef35b0693d7121e2da7e4d259ba77cf7985c142
prijatelj <theomnipotentwoof@gmail.com>
2020-04-14 19:24:59 -0400

    updated crc run scripts for lstm.

--- run_lstm_8M_crc.sh
+++ run_lstm_8M_crc.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 #$ -pe smp 4
-#$ -N lstm_4w_4u
-#$ -q gpu
-#$ -l gpu_card=1
-#$ -o crc_logs/lstm/10K/
-#$ -e crc_logs/lstm/10K/
+#$ -N lstm_8M_8w8u
+#$ -o crc_logs/lstm/
+#$ -e crc_logs/lstm/
 
 BASE_PATH="$HOME/Public/arch_2020_project"
 
@@ -14,9 +12,9 @@ module add conda
 conda activate tf-1.15
 
 python3 "$BASE_PATH/lstm.py" \
-    "$BASE_PATH/trace_files/gcc-10K.txt" \
-    -o "$BASE_PATH/results/lstm/10k/preds_lstm-4w-4u.csv" \
-    --window_size 4 \
+    "$BASE_PATH/trace_files/gcc-8M.txt" \
+    -o "$BASE_PATH/results/lstm/preds_8M_lstm-w8-u8.csv" \
+    --window_size 1 \
     --units 4 \
     --cpu_cores 4 \
     --cpu 1 \

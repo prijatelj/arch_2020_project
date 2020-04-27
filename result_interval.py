f9e1599dc43ee89fa5a7db3cb140140e13186921
Kevin Y.S. Wong <kevin.ysh.wong@gmail.com>
2020-04-26 21:49:34 -0400

    add results

--- result_interval.py
+++ result_interval.py
@@ -25,8 +25,9 @@ def interval_result(target, pred, metric_func, num_intervals):
 
 if __name__ == '__main__':
   program_dir_path = os.path.dirname(os.path.realpath(__file__))
-  trace_dir_path = program_dir_path + "/trace_files/"
+  trace_dir_path   = program_dir_path + "/trace_files/"
   results_dir_path = program_dir_path + "/results/"
+  output_dir_path  = results_dir_path + "metrics/"
 
   model_result_filename_prefix = {"perceptron": "perceptron-",
                                   "slp": "neural-net-single-",
@@ -34,9 +35,11 @@ if __name__ == '__main__':
                                   "lstm": "LSTM-pytorch-",
                                   "gru": "GRU-pytorch-"}
 
+  output_file = open(output_dir_path + "metrics_interval.txt", "w")
+
   for model in ["perceptron", "slp", "mlp", "lstm", "gru"]:
-    # for file_size in ["56", "10K", "8M"]:
     for file_size in ["56", "10K"]:
+    # for file_size in ["8M"]:
       for metric in [accuracy_score, f1_score, mutual_info_score, confusion_matrix]:
         trace_filename = "gcc-" + file_size + ".txt"
         trace_filepath = trace_dir_path + trace_filename
@@ -45,16 +48,10 @@ if __name__ == '__main__':
         trace_features, trace_labels = data_loader.get_data(data_path=trace_filepath, k=8)
         result_labels = result_data_loader.get_data(data_path=results_filepath)
 
-        print("***> " + model + " " + file_size + " " + metric.__name__)
-        print(interval_result(trace_labels, result_labels, metric, 4))
-        print()
-        print()
+        metric_output = interval_result(trace_labels, result_labels, metric, 4)
 
+        output_file.write("***> " + model + " " + file_size + " " + metric.__name__ + ":\n")
+        output_file.write(str(metric_output))
+        output_file.write("\n\n\n")
 
-
-
-
-
-
-
-
+  output_file.close()
--- result_overall.py
+++ result_overall.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
   program_dir_path = os.path.dirname(os.path.realpath(__file__))
   trace_dir_path = program_dir_path + "/trace_files/"
   results_dir_path = program_dir_path + "/results/"
+  output_dir_path  = results_dir_path + "metrics/"
 
   model_result_filename_prefix = {"perceptron": "perceptron-",
                                   "slp": "neural-net-single-",
@@ -22,9 +23,11 @@ if __name__ == '__main__':
                                   "lstm": "LSTM-pytorch-",
                                   "gru": "GRU-pytorch-"}
 
+  output_file = open(output_dir_path + "metrics_overall.txt", "w")
+
   for model in ["perceptron", "slp", "mlp", "lstm", "gru"]:
-    for file_size in ["56", "10K", "8M"]:
-    # for file_size in ["56", "10K"]:
+    for file_size in ["56", "10K"]:
+    # for file_size in ["8M"]:
       for metric in [accuracy_score, f1_score, mutual_info_score, confusion_matrix]:
         trace_filename = "gcc-" + file_size + ".txt"
         trace_filepath = trace_dir_path + trace_filename
@@ -33,16 +36,11 @@ if __name__ == '__main__':
         trace_features, trace_labels = data_loader.get_data(data_path=trace_filepath, k=8)
         result_labels = result_data_loader.get_data(data_path=results_filepath)
 
-        print("***> " + model + " " + file_size + " " + metric.__name__)
-        print(overall_result(trace_labels, result_labels, metric))
-        print()
-        print()
+        metric_output = overall_result(trace_labels, result_labels, metric)
 
+        overall_result(trace_labels, result_labels, metric)
+        output_file.write("***> " + model + " " + file_size + " " + metric.__name__ + ":\n")
+        output_file.write(str(metric_output))
+        output_file.write("\n\n\n")
 
-
-
-
-
-
-
-
+  output_file.close()

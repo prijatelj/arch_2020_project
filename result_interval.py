a1976e723c135087a976a1d248c09075263f59f9
Kevin Y.S. Wong <kevin.ysh.wong@gmail.com>
2020-04-27 14:18:33 -0400

    update interval result

--- result_interval.py
+++ result_interval.py
@@ -38,16 +38,15 @@ if __name__ == '__main__':
   output_file = open(output_dir_path + "metrics_interval.txt", "w")
 
   for model in ["perceptron", "slp", "mlp", "lstm", "gru"]:
-    for file_size in ["56", "10K"]:
-    # for file_size in ["8M"]:
+    for file_size in ["56", "10K", "8M"]:
+      trace_filename = "gcc-" + file_size + ".txt"
+      trace_filepath = trace_dir_path + trace_filename
+      results_filepath = results_dir_path + model_result_filename_prefix[model] + trace_filename
+
+      trace_features, trace_labels = data_loader.get_data(data_path=trace_filepath, k=8)
+      result_labels = result_data_loader.get_data(data_path=results_filepath)
+
       for metric in [accuracy_score, f1_score, mutual_info_score, confusion_matrix, matthews_corrcoef]:
-        trace_filename = "gcc-" + file_size + ".txt"
-        trace_filepath = trace_dir_path + trace_filename
-        results_filepath = results_dir_path + model_result_filename_prefix[model] + trace_filename
-
-        trace_features, trace_labels = data_loader.get_data(data_path=trace_filepath, k=8)
-        result_labels = result_data_loader.get_data(data_path=results_filepath)
-
         metric_output = interval_result(trace_labels, result_labels, metric, 4)
 
         output_file.write("***> " + model + " " + file_size + " " + metric.__name__ + ":\n")
@@ -59,23 +58,22 @@ if __name__ == '__main__':
   # (m,n) baseline
   output_file_mn = open(output_dir_path + "metrics_interval_mn.txt", "w")
 
-  for file_size in ["56", "10K"]:
-  # for file_size in ["8M"]:
+  for file_size in ["56", "10K", "8M"]:
+    trace_filename = "gcc-" + file_size + ".txt"
+    trace_filepath = trace_dir_path + trace_filename
+
+    results_filepath = ""
+    if file_size == "56":
+      results_filepath = results_dir_path + "python-gcc-56-(3,1).dat"
+    elif file_size == "10K":
+      results_filepath = results_dir_path + "python-gcc-10K-(4,1).dat"
+    else:
+      results_filepath = results_dir_path + "python-gcc-8M-(6,2).dat"
+
+    trace_features, trace_labels = data_loader.get_data(data_path=trace_filepath, k=8)
+    result_labels = result_data_loader.get_data(data_path=results_filepath)
+
     for metric in [accuracy_score, f1_score, mutual_info_score, confusion_matrix, matthews_corrcoef]:
-      trace_filename = "gcc-" + file_size + ".txt"
-      trace_filepath = trace_dir_path + trace_filename
-
-      results_filepath = ""
-      if file_size == "56":
-        results_filepath = results_dir_path + "python-gcc-56-(3,1).dat"
-      elif file_size == "10K":
-        results_filepath = results_dir_path + "python-gcc-10K-(4,1).dat"
-      else:
-        results_filepath = results_dir_path + "python-gcc-8M-(6,2).dat"
-
-      trace_features, trace_labels = data_loader.get_data(data_path=trace_filepath, k=8)
-      result_labels = result_data_loader.get_data(data_path=results_filepath)
-
       metric_output = interval_result(trace_labels, result_labels, metric, 4)
 
       output_file_mn.write("***> " + file_size + " " + metric.__name__ + ":\n")
